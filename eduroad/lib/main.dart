916e175999b02ebd4dac879434985062cb893186
Eliseo Alcaraz <elipalcaraz@gmail.com>
2025-02-24 23:14:29 +0800

    refactor(main): adjust the styles of the welcome page, depracated values are present (withOpacity)

--- eduroad/lib/main.dart
+++ eduroad/lib/main.dart
@@ -2,13 +2,13 @@ import 'package:eduroad/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:ui';
 
 void main() async {
   runApp(const MyApp());
 
   await dotenv.load(fileName: ".env");
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -28,107 +28,127 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text.rich(
-          TextSpan(
-            style: GoogleFonts.bricolageGrotesque(
-                fontSize: 24, fontWeight: FontWeight.bold),
-            children: const [
-              TextSpan(
-                text: 'ed',
-                style: TextStyle(color: Color(0xFFF78000)),
-              ),
-              TextSpan(
-                text: 'R',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Color(0xFF121212), Color(0x752A1E11), Color(0xFFF78000)],
+          stops: [0.0, 0.46, 1.0],
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.white,
-                side: const BorderSide(color: Colors.white, width: 1),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16)),
-              ),
-              onPressed: () {},
-              child: const Text('Sign in'),
-            ),
-          ),
-        ],
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF14110E), Color(0xFFF78000)],
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 120), // Space for the AppBar
-              Text(
-                "Welcome....",
-                style: GoogleFonts.bricolageGrotesque(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 16),
-              // Search Bar
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                height: 55,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
+      child: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Search ...",
-                          hintStyle: GoogleFonts.bricolageGrotesque(
-                              color: Colors.white70, fontSize: 18),
-                          border: InputBorder.none,
-                        ),
-                        style: const TextStyle(color: Colors.white),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'ed',
+                            style: GoogleFonts.bricolageGrotesque(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'R',
+                            style: GoogleFonts.bricolageGrotesque(
+                              fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const Icon(Icons.search, color: Colors.white),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        side: BorderSide(
+                            color: const Color.fromARGB(255, 94, 81, 81),
+                            width: 2.0),
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                      ),
+                      onPressed: () {},
+                      child: Text('Sign in',
+                          style: TextStyle(fontWeight: FontWeight.w800)),
+                    ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 24),
-              // Content placeholder
-              Expanded(
-                child: Center(
-                  child: Text(
-                    "Content Goes Here",
-                    style: GoogleFonts.bricolageGrotesque(
-                      fontSize: 20,
-                      color: Colors.white70,
+                SizedBox(height: 30),
+                Container(
+                  margin: EdgeInsets.only(left: 15),
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Welcome...',
+                      style: GoogleFonts.bricolageGrotesque(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            left: 30,
+            right: 30,
+            bottom: 100,
+            child: Material(
+              color: Colors.transparent,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.05),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.25),
+                      offset: Offset(0, 4),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search...",
+                        hintStyle: GoogleFonts.bricolageGrotesque(
+                          fontSize: 18,
+                          color: const Color.fromARGB(255, 196, 196, 196).withOpacity(0.6),
+                          fontWeight: FontWeight.w400,
+                        ),
+                        suffixIcon: Icon(Icons.search,
+                            color: Colors.white.withOpacity(0.6)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.05),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 35),
+                      ),
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
